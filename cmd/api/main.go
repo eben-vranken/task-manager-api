ba7f221fe0d946235657bab9486907b06ec4361b
eben-vranken <vranken.eben@outlook.com>
2026-05-20 23:15:58 +0200

    feat: add logging middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,29 +5,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/eben-vranken/task-manager-api/internal/database"
 	"github.com/eben-vranken/task-manager-api/internal/handlers"
 	"github.com/eben-vranken/task-manager-api/internal/repository"
 )
 
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (sr *statusRecorder) WriteHeader(code int) {
+	sr.statusCode = code
+	sr.ResponseWriter.WriteHeader(code)
+}
+
 const PORT string = "8080"
 
 var databaseURL = os.Getenv("DATABASE_URL")
 
 func main() {
-	http.HandleFunc("GET /health", healthCheck)
+	http.HandleFunc("GET /health", loggingMiddleware(healthCheck))
 
 	db, err := database.New(databaseURL)
-
-	taskRepository := &repository.TaskRepository{DB: db}
-	taskHandler := &handlers.TaskHandler{TR: taskRepository}
 
 	if err != nil {
 		log.Panicf("Error when opening database %v\n", err)
 	}
 
-	http.HandleFunc("POST /task", taskHandler.Create)
+	taskRepository := &repository.TaskRepository{DB: db}
+	taskHandler := &handlers.TaskHandler{TR: taskRepository}
+
+	http.HandleFunc("POST /task", loggingMiddleware(taskHandler.Create))
 
 	fmt.Println("Listening to port", PORT+"...")
 	log.Fatal(http.ListenAndServe("127.0.0.1:"+PORT, nil))
@@ -35,4 +46,20 @@ func main() {
 
 func healthCheck(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Up and running!"))
+}
+
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Println(req.URL.Path, "Initializing logging middleware")
+		start := time.Now()
+
+		recorder := &statusRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, req)
+		duration := time.Since(start)
+		log.Printf("[%s] %s %s %d", req.Method, req.RequestURI, duration, recorder.statusCode)
+	})
 }
